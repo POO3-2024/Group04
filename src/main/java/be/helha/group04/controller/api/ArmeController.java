d1b69095a6f35db73e2eb6a2f92f06da8e525192
Dedecker Bastien <127665643+la228797@users.noreply.github.com>
2024-05-15 14:58:51 +0200

    pré écriture de l'api arme

--- src/main/java/be/helha/group04/controller/api/ArmeController.java
+++ src/main/java/be/helha/group04/controller/api/ArmeController.java
@@ -1,28 +1,57 @@
 package be.helha.group04.controller.api;
 
 
+import be.helha.group04.arme.Arme;
+import be.helha.group04.modele.service.PersonnageService;
+import be.helha.group04.personnage.Personnage;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/arme")
 public class ArmeController {
+/*
+    @Autowired
+    //private ArmeService armeService;
 
-    /*@Autowired
-    private ArmeService armeService;
-
-    @GetMapping
-    public List<Personnage> getAllArmes() {
-        return armeService.getAllArmes();
+    public ArmeController() {
+        this.personnageService = new PersonnageService();
     }
 
+    //créer un nouveau personnage
+    @PostMapping
+    public Arme createArme(@RequestBody Arme arme){
+        //ArmeService.createArme(arme);
+        return arme;
+    }
+
+    //obtenir un personnage via son id
     @GetMapping("/{id}")
-    public Personnage getArme(@PathVariable("id") Integer id) {
+    public Arme getArmeById(@PathVariable("id") Integer id) {
         return armeService.getArmeById(id);
-    }*/
+    }
+
+    //obtenir toutes les personnages
+    @GetMapping
+    public List<Arme> getAllArme() {
+        return armeService.getAllArme();
+    }
+
+    //update les personnages
+    @PutMapping("/{id}")
+    public Arme updateArme(@PathVariable Integer id, @RequestBody Arme ArmeUpdated) {
+        ArmeUpdated.setId(id);
+        armeService.updateArme(ArmeUpdated);
+        return ArmeUpdated;
+    }
+
+    //supprimer un personnage via son id
+    @DeleteMapping("/{id}")
+    public void deleteArmeById(@PathVariable Integer id) {
+        armeService.deleteArmeById(id);
+    }
+
+ */
 }
