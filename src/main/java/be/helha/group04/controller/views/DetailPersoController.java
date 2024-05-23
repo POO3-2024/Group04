7adfefb161a5cc690602ae170a1aa83f7db30897
Collart Clara <la226237@student.helha.be>
2024-05-23 13:02:25 +0200

    javadoc detailPerso

--- src/main/java/be/helha/group04/controller/views/DetailPersoController.java
+++ src/main/java/be/helha/group04/controller/views/DetailPersoController.java
@@ -14,27 +14,53 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * cette classe reprèsente le controller de la vue dédié à la page de détail d'un personnage
+ * @author Clara
+ * @see be.helha.group04.controller.views
+ */
 public class DetailPersoController {
 
+    /**
+     * ce label reprèsente l'id du personnage
+     */
     @FXML
     Label id_perso;
-
+    /**
+     * ce label reprèsente le nom du personnage
+     */
     @FXML
     Label nom_perso;
+    /**
+     * ce label repèsente les pv du personnage
+     */
 
     @FXML
     Label pv;
-
+    /**
+     * ce label reprèsente la mana du personnage
+     */
     @FXML
     Label mana;
 
+    /**
+     * une instance de personnageController
+     */
     private PersonnageController personnageController;
 
-
+    /**
+     * consctructeur de la classe DetailPersoController
+     * initialise l'instance de personnageController
+     */
     public DetailPersoController() {
         this.personnageController = new PersonnageController();
     }
 
+    /**
+     * cette méthode est appelé avant l'affichage de la page pour
+     * pouvoir récupérer les bonnes informations à afficher
+     * @param id est l'id du personnage que l'on veut afficher les détails
+     */
     public void setId_perso(int id){
         Personnage personnageInfo = personnageController.getPersonnageById(id);
         id_perso.setText(String.valueOf(personnageInfo.getId()));
@@ -44,6 +70,10 @@ public class DetailPersoController {
     }
 
 
+    /**
+     * Réaffiche la page de détails des personnages, c'est un retour en arrière sur la page précedentes
+     * @param event est l'événement qui déclenche l'appel de cette méthode.
+     */
     @FXML
     public void retourListPerso(Event event){
         try {
